b4112373025cd55bbed2c7ef15760f687ce2d963
monickamagesh <monickamagesh@gmail.com>
2023-11-19 17:08:20 +0530

    contact

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function Services() {
+    return ( 
+        <main   className='py-32 pt-24 my-10 w-5/6 mx-auto h-auto'>
+      <div className=' bold justify-center items-center '>Contact page</div>
+      <div className='normal justify-center items-center '> Go Back to <span className='btn p-2'><Link href={'/'}>HOME</Link>  </span></div>
+
+
+    </main>
+    )
+}
