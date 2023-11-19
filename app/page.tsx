ab4ace6a243d492373551151a303a35d6027f141
monickamagesh <monickamagesh@gmail.com>
2023-11-19 18:23:00 +0530

    Case Study

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/hero";
 import Slider from "@/components/home/slider";
+import CaseStudy from "@/components/home/caseStudy";
 
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <CaseStudy />
       <Slider />
     </main>
   )
