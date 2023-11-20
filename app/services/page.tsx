a7a70af033b79d0ed8834d9bd96dcd1b0ab4a9b8
monickamagesh <monickamagesh@gmail.com>
2023-11-20 14:07:04 +0530

    Services

---
+++ app/services/page.tsx
@@ -0,0 +1,12 @@
+import FreeAssessment from "@/components/freeAssessment";
+
+
+
+export default function Services() {
+return (
+    <main>
+    <FreeAssessment />
+    
+    </main>
+)
+}
--- app/sevices/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Services() {
-    return ( 
-        <main className="pt-20">
-            
-        </main>
-    )
-}     
