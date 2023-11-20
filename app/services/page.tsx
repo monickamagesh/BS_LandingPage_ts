8335b9f10271c42007868750db8640d94a2fc4e5
monickamagesh <monickamagesh@gmail.com>
2023-11-20 14:17:45 +0530

    Services Hero Page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,10 +1,12 @@
 import FreeAssessment from "@/components/freeAssessment";
+import ServicesHero from "@/components/services/servicesHero";
 
 
 
 export default function Services() {
 return (
     <main>
+    <ServicesHero />
     <FreeAssessment />
     
     </main>
